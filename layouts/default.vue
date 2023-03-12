14a44e881be278bbba47de4b035fd2616d629140
neil-cartwright <neil@sitepages.co.uk>
2023-03-12 17:36:26 +0000

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,30 @@
+<template>
+ <div>
+  <header>
+   <nav>
+    <ul>
+     <li>
+      <NuxtLink to="/">Home</NuxtLink>
+     </li>
+     <li>
+      <NuxtLink to="/about">About</NuxtLink>
+     </li>
+     <li>
+      <NuxtLink to="/products">Products</NuxtLink>
+     </li>
+    </ul>
+   </nav>
+  </header>
+  <div>
+   <slot />
+  </div>
+ </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.router-link-exact-active {
+ color: blue;
+}
+</style>
